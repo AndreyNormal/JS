84f48866e24659db6639383a53fa427b002bbac9
AndreyNormal <69591303+AndreyNormal@users.noreply.github.com>
2021-03-13 22:10:32 +0500

    Add files via upload

---
+++ HW2/HW2.js
@@ -0,0 +1,49 @@
+let printApples = 0;
+
+console.log("your apples" + printApples + "tg.")
+
+if(printApples  > 0){
+	console.loog("everything Ok");
+
+}else if (printApples  ===0){
+	console.log("empty");
+
+}else {
+	console.log("you don't have apples ")
+}
+printApples  +=400;
+console.log("your have 10 apples  " + printApples  + "tg.");
+
+
+if(printApples  > 0){
+	console.log("everything Ok");
+
+}else if (printApples  === 0){
+	console.log("empty");
+}else{
+	console.log("you don't have apples")
+}
+
+printApples  -= 900;
+console.log("your have 30 apples " + printApples  + "tg.");
+
+if(printApples  > 0){
+	console.log("everything Ok");
+} else if(printApples  === 0);{
+
+}else {
+	console.log("you don't have apples ");
+}
+let a ="c"
+a="b";
+a+ 1;
+
+if(true){
+	a ="a"
+	console.log(a);
+}
+if(true) {
+	let a ="b"
+	console.log(a);
+}
+
