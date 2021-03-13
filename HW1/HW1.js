3f7131ca47b5deb853217ed3cc1b4ead763c209a
AndreyNormal <69591303+AndreyNormal@users.noreply.github.com>
2021-03-13 20:56:32 +0500

    Add files via upload

---
+++ HW1/HW1.js
@@ -0,0 +1,42 @@
+let countApple = 0;
+
+console.log("Amount Apple =" + countApple);
+
+if(countApple > 0){
+	console.log("Omg! Do you have something");
+
+}
+else if(countApple === 0){
+	console.log("Friend you  don't have apples");
+}
+else{
+	console.log("ERROR");
+}
+
+countApple += 100;
+
+console.log("Amount apple =" + countApple);
+
+if(countApple > 0){
+	console.log("Omg! Do you have apples?")
+}
+else if(countApple === 0){
+	console.log("friend doesn't have apples.Go and buy")
+}
+else{
+	console.log("are you Ok?")
+}
+
+countApple -=1000
+
+console.log("Amount apple =" + countApple);
+
+if(countApple > 0){
+	console.log("Omg! Do you have apples?");
+}
+else if(countApple === 0){
+	console.log("friend doesn't have apples.Go and buy");
+}
+else{
+	console.log("You lost")
+}
